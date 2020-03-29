fe7b03f211ffa7b5430262dfe66f2a6c750d1671
ALONPC <alon.berrios@gmail.com>
2020-03-29 01:56:08 -0300

    Connection to mongoDB and movie schema

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@
 const fastify = require("fastify")({
   logger: true
 });
+const mongoose = require("mongoose");
+
+// Connect to DB
+const connectDb = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost/movies");
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("An error occurred when trying to connect to database");
+  }
+};
+connectDb();
+
+// Same as this...
+// mongoose
+//   .connect("mongodb://localhost/movies")
+//   .then(() => {
+//     console.log("MongoDB connected...");
+//   })
+//   .catch(error => {
+//     console.log("An error ocurred", error);
+//   });
+
+// Test object
+const movie = {
+  title: "1917",
+  year: 2019,
+  genres: ["Drama", "War"],
+  director: "Sam Mendes",
+  duration: { measure: "minutes", quantity: 119 }
+};
 
 // Declare a route
-fastify.get("/", async (request, reply) => ({ hello: "world" }));
+fastify.get("/movies", async (request, reply) => [movie]);
 
 // Running the server
 const start = async () => {
