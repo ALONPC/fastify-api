d5db132ce059e6f85472040f5f586576fe20f8f7
ALONPC <alon.berrios@gmail.com>
2020-03-29 02:06:07 -0300

    Movie controller

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,28 +1,66 @@
 import boom from "boom";
 import Movie from "../models/Movie";
 
-const boomifyError = (error) => { throw boom.boomify(error) }
+// Make boom to handle errors
+const boomifyError = error => {
+  throw boom.boomify(error);
+};
 
 // Get all movies
-exports.getMovies = async((req, res) => {
-    try {
-        const movies = await Movie.find()
-        console.log("exports.getMovies -> movies", movies)
-        return movies;
-    } catch(error) {
-        boomifyError(error);
-    }
-})
+exports.getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    console.log("exports.getMovies -> movies", movies);
+    return movies;
+  } catch (error) {
+    boomifyError(error);
+  }
+};
 
 // Get single movie by id
-exports.getSingleMovie = async ((req, res) => {
-    try {
-        const id = req.params.id;
-        console.log("exports.getSingleMovie -> id", id)
-        const movie = await Movie.findById(id)
-        console.log("exports.getSingleMovie -> movie", movie)
-        return movie;
-    } catch (error) {
-        boomifyError(error);
-    }
-})
+exports.getSingleMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log("exports.getSingleMovie -> id", id);
+    const movie = await Movie.findById(id);
+    console.log("exports.getSingleMovie -> movie", movie);
+    return movie;
+  } catch (error) {
+    boomifyError(error);
+  }
+};
+
+// Add a new movie
+exports.addMovie = async (req, res) => {
+  try {
+    const movie = new Movie(req.body);
+    console.log("exports.addMovie -> movie", movie);
+    return movie.save();
+  } catch (err) {
+    boomifyError(error);
+  }
+};
+
+// Update an existing movie
+exports.updateMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const movie = req.body;
+    const { ...updateData } = movie;
+    const update = await Movie.findByIdAndUpdate(id, updateData, { new: true });
+    return update;
+  } catch (err) {
+    boomifyError(error);
+  }
+};
+
+// Delete a movie
+exports.deleteCar = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const movie = await Movie.findByIdAndRemove(id);
+    return movie;
+  } catch (err) {
+    boomifyError(error);
+  }
+};
